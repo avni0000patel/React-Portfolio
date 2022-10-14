be11bebbf94161148a6285d05b4a4750f74abcb6
Avni <avni0000patel@gmail.com>
2022-10-13 21:59:02 -0400

    css sticky footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,11 @@ export default function Footer() {
     const styles = {
         color: {
             backgroundColor: '#274C77',
+            textAlign: "center",
+            position: "fixed",
+            left: "0",
+            bottom: "0",
+            width: "100%"
         },
     };
     return (
