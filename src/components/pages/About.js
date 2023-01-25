2b2bac797c28b554e524352a830335959a2dccb1
Avni <avni0000patel@gmail.com>
2023-01-25 18:08:23 -0500

    change bio in about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -21,8 +21,13 @@ export default function About() {
             </div>
             <div>
                 <div className="bio">
-                    Welcome to my story Software Engineer! I have a background in finance and mathematics, as well as training from University of Pennsylvania in full stack web development. I have experience building projects using JavaScript, Node, SQL, MongoDB, React, and other technologies.
-                </div>
+                    As a software engineer, my passion lies in solving complex problems and creating innovative solutions. My background in finance and mathematics, coupled with my training from the University of Pennsylvania in full stack web development, has equipped me with a unique set of skills and a strong understanding of both the technical and business aspects of software development.
+                    <br />
+                    <br />
+                    I have experience building projects using a variety of technologies, including JavaScript, Node, SQL, MongoDB, and React. My expertise in these technologies, along with my ability to adapt to new technologies quickly, enables me to deliver high-quality and efficient solutions that meet the needs of my clients.
+                    <br />
+                    <br />
+                    I am always eager to take on new challenges, and I am continuously learning and growing as a software engineer. I am excited to share my skills, knowledge and experience to help others to achieve their goals and bring their ideas to life. Welcome to my portfolio and I hope you enjoy exploring my work as much as I enjoyed creating it.                </div>
             </div>
         </section >
     );
